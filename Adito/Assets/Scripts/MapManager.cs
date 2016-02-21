8c25e3f343a4f9ae81f6587174b06236b34b285c
Daniel <daniel_fleischer86@web.de>
2016-02-21 18:40:10 +0100

    neue Sachen

--- Adito/Assets/Scripts/MapManager.cs
+++ Adito/Assets/Scripts/MapManager.cs
@@ -3,24 +3,29 @@ using System.Collections.Generic;
 
 public class MapManager : MonoBehaviour
 {
+    private MapShape[,] mapArr;
+
 
     public List<GameObject> prefStaticObjects;
 
     public GameObject prefMapManagerEmptyObj;
     public GameObject prefFloor;
     public GameObject prefWall;
+    public GameObject prefPlayer;
 
-    public int mapWidth = 16;
-    public int mapHeigth = 12;
 
-    float imageSize = 50;
+    private int mapWidth = 18;
+    private int mapHeigth = 14;
+    private float imageSize = 50;
 
 
     // Use this for initialization
     void Start()
     {
-        CreateMap();
-        CreateStaticObjects();
+        mapArr = new MapShape[mapWidth, mapHeigth];
+
+        CreateNewMap();
+        AddPlayer();
     }
 
     // Update is called once per frame
@@ -29,65 +34,120 @@ public class MapManager : MonoBehaviour
 
     }
 
-    void CreateStaticObjects()
+
+    void placeWallsDummy()
     {
-        float posY = 0f;
-        float posX = 0f;
-        float space = imageSize / 100f;
+        mapArr[2, 2].Prefab = prefStaticObjects[0];
+        mapArr[2, 3].Prefab = prefStaticObjects[0];
+        mapArr[2, 4].Prefab = prefStaticObjects[0];
+        mapArr[2, 9].Prefab = prefStaticObjects[0];
+        mapArr[2, 10].Prefab = prefStaticObjects[0];
+        mapArr[2, 11].Prefab = prefStaticObjects[0];
 
-        for (int i = 0; i < 15; i++)
-        {
-            float p1 = Random.Range(0, mapHeigth);
-            float p2 = Random.Range(0, mapWidth);
+        mapArr[3, 2].Prefab = prefStaticObjects[0];
+        mapArr[3, 11].Prefab = prefStaticObjects[0];
+        mapArr[4, 2].Prefab = prefStaticObjects[0];
+        mapArr[4, 11].Prefab = prefStaticObjects[0];
 
-            GameObject g = (GameObject)Instantiate(prefStaticObjects[0], new Vector3(p2 * space, p1 * space), Quaternion.identity);
-            g.transform.parent = prefMapManagerEmptyObj.transform;
-        }
+        mapArr[5, 9].Prefab = prefStaticObjects[0];
+        mapArr[6, 9].Prefab = prefStaticObjects[0];
+        mapArr[7, 9].Prefab = prefStaticObjects[0];
+        mapArr[5, 4].Prefab = prefStaticObjects[0];
+        mapArr[6, 4].Prefab = prefStaticObjects[0];
+        mapArr[7, 4].Prefab = prefStaticObjects[0];
+        mapArr[7, 6].Prefab = prefStaticObjects[0];
+        mapArr[7, 7].Prefab = prefStaticObjects[0];
+        mapArr[10, 9].Prefab = prefStaticObjects[0];
+        mapArr[11, 9].Prefab = prefStaticObjects[0];
+        mapArr[12, 9].Prefab = prefStaticObjects[0];
+        mapArr[10, 7].Prefab = prefStaticObjects[0];
+        mapArr[10, 6].Prefab = prefStaticObjects[0];
+        mapArr[10, 4].Prefab = prefStaticObjects[0];
+        mapArr[11, 4].Prefab = prefStaticObjects[0];
+        mapArr[12, 4].Prefab = prefStaticObjects[0];
+        mapArr[13, 11].Prefab = prefStaticObjects[0];
+        mapArr[14, 11].Prefab = prefStaticObjects[0];
+        mapArr[15, 11].Prefab = prefStaticObjects[0];
+        mapArr[15, 10].Prefab = prefStaticObjects[0];
+        mapArr[15, 9].Prefab = prefStaticObjects[0];
+        mapArr[13, 2].Prefab = prefStaticObjects[0];
+        mapArr[14, 2].Prefab = prefStaticObjects[0];
+        mapArr[15, 2].Prefab = prefStaticObjects[0];
+        mapArr[15, 3].Prefab = prefStaticObjects[0];
+        mapArr[15, 4].Prefab = prefStaticObjects[0];
+
+
+    }
+    void AddPlayer()
+    {
+        Instantiate(prefPlayer, mapArr[1, 1].Position, Quaternion.Euler(0, 0, 0));
     }
 
 
-    void CreateMap()
+    void CreateNewMap()
     {
-        float posY = 0f;
-        float posX = 0f;
-        float space = imageSize / 100f;
+        float posX = 0;
+        float posY = 0;
+        float imageSpace = imageSize / 100f;
+
+        //fill Arr
+        for (int y = 0; y < mapHeigth; y++)
+        {
+            for (int x = 0; x < mapWidth; x++)
+            {
+                mapArr[x, y] = new MapShape(prefFloor, new Vector2(posX, posY), imageSize);
+
+                if (x == 0)
+                {
+                    mapArr[x, y] = new MapShape(prefWall, new Vector2(posX, posY), imageSize, 90);
+                }
+                if (y == 0)
+                {
+                    mapArr[x, y] = new MapShape(prefWall, new Vector2(posX, posY), imageSize, 180);
+                }
+                if (x == mapWidth - 1)
+                {
+                    mapArr[x, y] = new MapShape(prefWall, new Vector2(posX, posY), imageSize, 270);
+                }
+                if (y == mapHeigth - 1)
+                {
+                    mapArr[x, y] = new MapShape(prefWall, new Vector2(posX, posY), imageSize);
+                }
+
+                posX += imageSpace;
+            }
+            posX = 0;
+            posY += imageSpace;
+        }
+
+        placeWallsDummy();
 
         for (int y = 0; y < mapHeigth; y++)
         {
             for (int x = 0; x < mapWidth; x++)
             {
-                if (y == 0)
-                {
-                    PlaceWalls(prefWall, posX, posY - space, 180);
-                }
-
-                if (y == mapHeigth - 1)
-                {
-                    PlaceWalls(prefWall, posX, posY + space, 0);
-                }
-
-                if (x == 0)
-                {
-                    PlaceWalls(prefWall, posX - space, posY, 90);
-                }
-
-                if (x == mapWidth - 1)
-                {
-                    PlaceWalls(prefWall, posX + space, posY, 270);
-                }
-
-                GameObject gFloor = (GameObject)Instantiate(prefFloor, new Vector3(posX, posY), Quaternion.identity);
-                gFloor.transform.parent = prefMapManagerEmptyObj.transform;
-                posX += space;
+                GameObject gObj = (GameObject)Instantiate(mapArr[x, y].Prefab, mapArr[x, y].Position, Quaternion.Euler(0, 0, mapArr[x, y].ImageRotation));
+                gObj.transform.parent = prefMapManagerEmptyObj.transform;
             }
-            posX = 0;
-            posY += space;
         }
     }
+     
+}
 
-    void PlaceWalls(GameObject obj, float posX, float posY, float rotationAngle)
+
+public class MapShape
+{
+    public Vector2 Position { get; set; }
+    public GameObject Prefab { get; set; }
+    public float Size { get; set; }
+    public int ImageRotation { get; set; }
+
+    public MapShape() { }
+    public MapShape(GameObject pref, Vector2 pos, float size, int imgRotation = 0)
     {
-        GameObject g = (GameObject)Instantiate(obj, new Vector3(posX, posY), Quaternion.Euler(0, 0, rotationAngle));
-        g.transform.parent = prefMapManagerEmptyObj.transform;
+        Position = pos;
+        Prefab = pref;
+        Size = size;
+        ImageRotation = imgRotation;
     }
 }
